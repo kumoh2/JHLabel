8aa3ff74bf45dbb6eb1bd4632378422187b56b4c
jaeho <31762986+kumoh2@users.noreply.github.com>
2025-04-10 08:00:56 +0900

    WEB 기반으로 리팩터링을 위한 기존 파일 제거

--- AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JHLabel;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-**Test Site**
-
-https://labelary.com/viewer.html
